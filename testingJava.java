7591f276eee1130c5e70da5a2a85282f6ea00616
Yenaro Joel Noa Camino <97652778+Diegoo11@users.noreply.github.com>
2023-04-27 01:03:18 +0000

    uwu

---
+++ testingJava.java
@@ -0,0 +1,21 @@
+//import java.util.*;
+//Esto no hace absolutamente nada
+public class testingJava {
+  public static void main(String[] args) {
+    //System.out.println("xd que mrd de lenguaje");
+    //int testNum = 1, otroTestNum = 2;
+    //String hola = "hola??";
+    //hola = "ax?";
+    //System.out.println(testNum + otroTestNum + hola);
+    //Scanner scan = new Scanner(System.in);
+    //int num = scan.nextInt();
+    //System.out.println(num);
+    //String firstInput = scan.nextLine();
+    //System.out.println("Esto es lo que escribiste en cosola " + firstInput);
+    String myName = "Diegoo";
+    String nameCut = myName.substring(3, 5);//go
+    System.out.println("original: " + myName + " / Cut: " + nameCut);
+    System.out.println("length1: "+ myName.length());
+    System.out.println(myName.indexOf("ieie"));
+  }
+}
