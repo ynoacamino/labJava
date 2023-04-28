b3923bb33fd5c9b4e3b1dd56e46dc59bc4e73e75
Diegoo11 <ynoacamino@gmail.com>
2023-04-28 17:50:55 -0500

    testing Java's sintaxs

--- testingJava.java
+++ testingJava.java
@@ -1,21 +1,93 @@
-//import java.util.*;
+import java.util.*;
 //Esto no hace absolutamente nada
 public class testingJava {
   public static void main(String[] args) {
-    //System.out.println("xd que mrd de lenguaje");
-    //int testNum = 1, otroTestNum = 2;
-    //String hola = "hola??";
-    //hola = "ax?";
-    //System.out.println(testNum + otroTestNum + hola);
-    //Scanner scan = new Scanner(System.in);
-    //int num = scan.nextInt();
-    //System.out.println(num);
-    //String firstInput = scan.nextLine();
-    //System.out.println("Esto es lo que escribiste en cosola " + firstInput);
-    String myName = "Diegoo";
-    String nameCut = myName.substring(3, 5);//go
-    System.out.println("original: " + myName + " / Cut: " + nameCut);
-    System.out.println("length1: "+ myName.length());
-    System.out.println(myName.indexOf("ieie"));
+    Scanner scan = new Scanner(System.in);
+    scan.close();
+    /*int sold = scan.nextInt();
+    scan.close();
+    if (sold >= 1000){
+      consoleLog("buen sueldo");
+    }
+    else{
+      consoleLog("mal sueldo");
+    }
+    consoleLog("nueva pruerba"); 
+    consoleLog("--------------------------");
+    int number = scan.nextInt();
+    scan.close();
+    switch (number) {
+      case 1:
+        consoleLog("uno");
+        break;
+      case 2:
+        consoleLog("dos");
+        break;
+      case 3:
+        consoleLog("tres");
+        break;
+      default:
+        consoleLog("cualquier otro numero");
+    }
+    consoleLog(subtract(2, 4));
+    consoleLog(subtract(7, 2));
+    
+    int numWhile = 1;
+    while (numWhile <= 10) {
+      consoleLog(numWhile);
+      numWhile += 1;
+    }
+    
+    for (int num = 1; num <= 10; num++) {
+      consoleLog(num);
+    }
+    
+    int num = 1;
+    do {
+      consoleLog(num);
+      num += 1;
+    }
+    while (num <= 10);
+    
+    //EJERCICIO PARA BUBLES
+    for(int i = 1; i <= 12; i++) {
+      consoleLog(i * 9);
+    }
+    
+    consoleLog("--------------------");
+
+    for(int i = 30; i <= 50; i++) {
+      if(i % 3 == 0) consoleLog(i);
+    }
+    */
+    //EJERCICIO MAS INTERESANTE: SIMULAR UN RELOJ
+    consoleLog("inicializa el reloj");
+    for(int segs = 0, min = 0, hours = 0; segs != 59 || min != 59 || hours != 23; segs++) {
+      if(segs == 60) {
+        segs = 0;
+        min += 1;
+      }
+      if(min == 60) {
+        min = 0;
+        hours += 1;
+      }
+      String timer = "";
+      timer = timer + hours + " : " + min + " : " + segs;
+      consoleLog(timer);
+    }
+    consoleLog("Termino el dia, son las: 23:59:59");
+  }
+  public static void consoleLog(String imp) {
+    System.out.println(imp);
+  }
+  public static void consoleLog(int imp) {
+    System.out.println(imp);
+  }
+  public static int subtract(int num1, int num2) {
+    if (num1 > num2) {
+      return num1 - num2;
+    } else {
+      return num2 - num1;
+    }
   }
 }
