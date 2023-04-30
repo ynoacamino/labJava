3f2a63737188725c54b9ba1003418db44ee852ed
Diegoo11 <ynoacamino@gmail.com>
2023-04-29 23:25:07 -0500

    add code lines of condicional and operators

--- exercises/operators.java
+++ exercises/operators.java
@@ -3,7 +3,15 @@ import java.util.Scanner;
 
 public class operators {
   public static void main(String[] args) {
-    
+    Scanner scan = new Scanner(System.in);
+    int sold = scan.nextInt();
+    scan.close();
+    if (sold >= 1000){
+      consoleLog("buen sueldo");
+    }
+    else{
+      consoleLog("mal sueldo");
+    }
   }
   //ConsoleLog in Java :b
   public static void consoleLog(String imp) {
