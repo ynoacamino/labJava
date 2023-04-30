40602dcb44ab0c841d9ba3d3f6f51f3530ee25ad
Diegoo11 <ynoacamino@gmail.com>
2023-04-29 23:24:47 -0500

    add execises of methods

--- exercises/metodos.java
+++ exercises/metodos.java
@@ -6,6 +6,20 @@ public class metodos {
     consoleLog(subtract(7, 2));
     consoleLog(mayor(1, 4));
     consoleLog("el 4 es mayor que el 10? " + mayorBoolean(4, 10));
+    consoleLog(mayorNums(1, 2, 3, 4));
+  }
+  
+  //Devuelve el mayot de cuatro numeros
+  public static int mayorNums(int num1, int num2, int num3, int num4) {
+    int arrayArg[] = {num1, num2, num3, num4};
+    for(int i = 0; i <= 4; i++) {
+      if(mayorBoolean(arrayArg[i], num1)
+        && mayorBoolean(arrayArg[i], num2)
+        && mayorBoolean(arrayArg[i], num3)
+        && mayorBoolean(arrayArg[i], num4)
+      ) return arrayArg[i];
+    }
+    return 0;
   }
   //Devuelve int de la resta de dos numero de manera positiva
   public static int subtract(int num1, int num2) {
