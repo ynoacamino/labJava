17ac66c2c52c74a1ee008edddb25a0a5815d240a
Diegoo11 <ynoacamino@gmail.com>
2023-04-29 23:04:19 -0500

    file created for organised

---
+++ exercises/operators.java
@@ -0,0 +1,17 @@
+package exercises;
+
+public class operators {
+  public static void main(String[] args) {
+    
+  }
+  //ConsoleLog in Java :b
+  public static void consoleLog(String imp) {
+    System.out.println(imp);
+  }
+  public static void consoleLog(int imp) {
+    System.out.println(imp);
+  }
+  public static void consoleLog(Boolean imp) {
+    System.out.println(imp);
+  }
+}
