d0f688afdff63b342ada8997d30d1b224b0745c9
Diegoo11 <ynoacamino@gmail.com>
2023-04-29 23:24:22 -0500

    add code lines of switch and if

--- exercises/condicionals.java
+++ exercises/condicionals.java
@@ -1,8 +1,24 @@
 package exercises;
+import java.util.Scanner;
 
 public class condicionals {
   public static void main(String[] args) {
-    
+    Scanner scan = new Scanner(System.in);
+    int number = scan.nextInt();
+    scan.close();
+    switch (number) {
+      case 1:
+        consoleLog("uno");
+        break;
+      case 2:
+        consoleLog("dos");
+        break;
+      case 3:
+        consoleLog("tres");
+        break;
+      default:
+        consoleLog("cualquier otro numero");
+    }
   }
   //ConsoleLog in Java :b
   public static void consoleLog(String imp) {
