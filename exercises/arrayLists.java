b816eff7448c795bc3d0c68cb4ec0c7825fbc9f2
Diegoo11 <ynoacamino@gmail.com>
2023-04-29 23:23:30 -0500

    add arraysList and methods

--- exercises/arrayLists.java
+++ exercises/arrayLists.java
@@ -1,8 +1,15 @@
 package exercises;
+import java.util.ArrayList;
 
 public class arrayLists {
   public static void main(String[] args) {
+    ArrayList<String> myArray = new ArrayList<String>();
+
+    myArray.add("3");
+    myArray.add("2");
+    myArray.add("1");
     
+    myArray.forEach(x -> consoleLog(x));
   }
   //ConsoleLog in Java :b
   public static void consoleLog(String imp) {
