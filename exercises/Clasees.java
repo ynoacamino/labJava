eabeed08759545549aa1dbe369dab08835170731
Diegoo11 <ynoacamino@gmail.com>
2023-05-01 17:00:52 -0500

    new file of how declaret class

---
+++ exercises/Clasees.java
@@ -0,0 +1,38 @@
+package exercises;
+
+
+public class Clasees {
+  private int old;
+  private String name;
+  private String lastName;
+
+  public Clasees(int old, String name, String lastName) {
+    setLastName(lastName);
+    setName(name);
+    setOld(old);
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getOld() {
+    return old;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setOld(int old) {
+    this.old = old;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+}
