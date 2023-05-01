eb87c60a38cc8568e3f550993ce3eb41d7c552ee
Diegoo11 <ynoacamino@gmail.com>
2023-05-01 17:00:31 -0500

    import class of file exercises

--- testingJava.java
+++ testingJava.java
@@ -1,9 +1,12 @@
 //Esto no hace absolutamente nada
+import exercises.Clasees;
+
 public class testingJava {
+
   public static void main(String[] args) {
-    
+    Clasees yo = new Clasees(12, "Yenaro", "Noa");
+    consoleLog(yo.getName());
   }
-  
   //ConsoleLog in Java :b
   public static void consoleLog(String imp) {
     System.out.println(imp);
