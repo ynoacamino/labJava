fc3c0392adb2298caa87eef2f08451d856091021
Diegoo11 <ynoacamino@gmail.com>
2023-05-24 19:37:45 -0500

    homework

---
+++ tareas/homework08/Programa1.java
@@ -0,0 +1,19 @@
+package tareas.homework08;
+import java.util.*;
+
+public class Programa1{
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Ingrese su sueldo base");
+    double sueldo = sc.nextInt();
+    sc.close();
+    
+    if(sueldo < 1000) {
+      sueldo = sueldo*1.1;
+    }
+
+    System.out.println("Su sueldo final es: " + sueldo);
+  }
+}
+
+
