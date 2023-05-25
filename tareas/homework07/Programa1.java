7ea9a192a58ea709fe0078c4093ddb0b8437cf59
Diegoo11 <ynoacamino@gmail.com>
2023-05-24 19:37:41 -0500

    homework

---
+++ tareas/homework07/Programa1.java
@@ -0,0 +1,14 @@
+package tareas.homework07;
+import java.util.*;
+
+class Programa1 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    
+    System.out.println("Indique el numero de estudiantes");
+    int nro = sc.nextInt();
+    sc.close();
+
+    System.out.println("El alumno escogido es: " + (int) (Math.random()*nro +1));
+  }
+}
