fae5dbfc808a5978aa3fd858112ea21b7cc5b3c0
Yenaro Joel Noa Camino <97652778+Diegoo11@users.noreply.github.com>
2023-05-29 23:03:11 +0000

    homework

---
+++ tareas/homework09/Programa1.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Programa1 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    double num1, num2, num3, average;
+    num1 = sc.nextDouble();
+    num2 = sc.nextDouble();
+    num3 = sc.nextDouble();
+    average = (num1 + num2 + num3)/3;
+
+    if(average >= 13) {
+      System.out.println("Aprobaste");
+    } else {
+      System.out.println("Desaprobaste");
+    }
+  }
+}
