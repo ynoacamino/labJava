794e4a72cdf7e0513a596e70808b4e41976b76cd
Yenaro Joel Noa Camino <97652778+Diegoo11@users.noreply.github.com>
2023-05-29 23:03:30 +0000

    homework

---
+++ tareas/homework09/Programa4.java
@@ -0,0 +1,20 @@
+//Programa que te dice si invertir o no
+import java.util.*;
+
+class Programa4 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    double capital, tasa, interes;
+    capital = sc.nextDouble();
+    tasa = sc.nextDouble()/100;
+    interes = capital * tasa;
+
+    if(interes > 200) {
+      System.out.println("Si debes invertir");
+      System.out.println("El monto final sera de: " + (capital + interes));
+    } else {
+      System.out.println("No debes invertir");
+    }
+  }
+}
