373e4384e8a4536f71911d47d7cca661e2dbc65b
Yenaro Joel Noa Camino <97652778+Diegoo11@users.noreply.github.com>
2023-05-29 23:03:18 +0000

    homework

---
+++ tareas/homework09/Programa2.java
@@ -0,0 +1,20 @@
+//Programa que imprime de manera asendente dos numeros ingresados
+import java.util.*;
+
+class Programa2 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int num1, num2;
+    num1 = sc.nextDouble();
+    num2 = sc.nextDouble();
+
+    if(num1 > num2) {
+      System.out.println(num2);
+      System.out.println(num1);
+    } else {
+      System.out.println(num1);
+      System.out.println(num2);
+    }
+  }
+}
