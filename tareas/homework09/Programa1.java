ba27c7c123823b4c45c945e9709f320a544bf1ba
Diegoo11 <ynoacamino@gmail.com>
2023-05-29 20:55:44 -0500

    homework

--- tareas/homework09/Programa1.java
+++ tareas/homework09/Programa1.java
@@ -11,6 +11,8 @@ class Programa1 {
     num3 = sc.nextDouble();
     average = (num1 + num2 + num3)/3;
 
+    sc.close();
+
     if(average >= 13) {
       System.out.println("Aprobaste");
     } else {
@@ -18,3 +20,6 @@ class Programa1 {
     }
   }
 }
+
+
+
