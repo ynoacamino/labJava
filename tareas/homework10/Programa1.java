38bb091a865f52437da2f804437184dbdb24a01c
Yenaro Joel Noa Camino <97652778+Diegoo11@users.noreply.github.com>
2023-06-02 19:13:19 +0000

    homework10

---
+++ tareas/homework10/Programa1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Programa10 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    double notaLab = sc.nextInt();
+    double notaTeo = sc.nextInt();
+
+    
+  }
+}
