bdcd230e4cc5f61afd9d9b5ca487e79def4009cd
Diegoo11 <ynoacamino@gmail.com>
2023-06-05 21:06:03 -0500

    homework11

---
+++ tareas/homework11/hw11Programa1_v1.java
@@ -0,0 +1,12 @@
+package tareas.homework11;
+//muestra la tabla de multiplicar del 9
+public class hw11Programa1_v1 {
+  public static void main(String[] args) {
+    int i = 1;
+    while(i <= 12){
+      System.out.println("9 por " + i + " es = " + i*9);
+      i += 1;
+    }
+  }
+}
+
