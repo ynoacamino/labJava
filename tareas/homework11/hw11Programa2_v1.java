ed68e04904f02f26ad1993446cfa2d34c1128591
Diegoo11 <ynoacamino@gmail.com>
2023-06-05 21:06:08 -0500

    homework11

---
+++ tareas/homework11/hw11Programa2_v1.java
@@ -0,0 +1,13 @@
+package tareas.homework11;
+//Programa q muestra los numeros inpares del 30 al 50
+public class hw11Programa2_v1 {
+  public static void main(String[] args) {
+    int i = 31;
+    while(i <= 50){
+      System.out.println(i);
+      i += 2;
+    }
+  }
+}
+
+
