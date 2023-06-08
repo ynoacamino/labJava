750f591edeb93d3314a92aa9e8d1589c955aabfe
Diegoo11 <ynoacamino@gmail.com>
2023-06-07 22:35:05 -0500

    tarea lab

---
+++ labs/Problemas/Programa2.java
@@ -0,0 +1,23 @@
+package Problemas;
+import java.util.*;
+
+public class Programa2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String adn = sc.nextLine();
+        int num = sc.nextInt();
+        String cadena = adn.substring(0, num);
+
+        int repet = 0;
+        int i = 0;
+        while( i < adn.length() - num ){
+            if( cadena.equals(adn.substring(i, i + num)) ) {
+                repet += 1;
+            }
+
+            i += 1;
+        }
+
+        System.out.println(repet);
+    }
+}
