750c39aadc8b7b0d77994b81f4eff507866898c9
Diegoo11 <ynoacamino@gmail.com>
2023-06-07 22:34:57 -0500

    tarea lab

---
+++ labs/Problemas/Problema1.java
@@ -0,0 +1,29 @@
+package Problemas;
+import java.util.*;
+
+public class Problema1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String cadena = sc.nextLine().toLowerCase();
+
+        int i = 0;
+        int cual = 0;
+        int como = 0;
+
+        while(i < cadena.length() - 4){
+            if( "como".equals(cadena.substring(i, i + 4)) ){
+                como += 1;
+            }
+            if( "cual".equals(cadena.substring(i, i + 4)) ){
+                cual += 1;
+            }
+            i += 1;
+        }
+
+        if(cual == como) {
+            System.out.println("Igual cantidad");
+        }else{
+            System.out.println("Distinta cantidad");
+        }
+    }
+}
