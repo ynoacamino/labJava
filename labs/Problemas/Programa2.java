bbc2614c4f7b15beb659cf7419aebbfcc69de887
Diegoo11 <ynoacamino@gmail.com>
2023-06-07 22:58:04 -0500

    tarea lab

--- labs/Problemas/Programa2.java
+++ labs/Problemas/Programa2.java
@@ -4,8 +4,10 @@ import java.util.*;
 public class Programa2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String adn = sc.nextLine();
         int num = sc.nextInt();
+        //linea agregada para evitar error con la captura por teclado del ENTER
+        sc.nextLine();
+        String adn = sc.nextLine();
         String cadena = adn.substring(0, num);
 
         int repet = -1;
