f4bdea7706302ca14a0adb6c39a3d81011de9b0a
Diegoo11 <ynoacamino@gmail.com>
2023-06-08 21:03:51 -0500

    homework12

---
+++ tareas/homework12/Programa04_v3.java
@@ -0,0 +1,15 @@
+package tareas.homework12;
+import java.util.*;
+
+public class Programa04_v3 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int min = sc.nextInt();
+    int max = sc.nextInt();
+    int sum = 0;
+
+    for(int i = min; i <= max; i += 1) {
+      sum += i;
+    }
+  }
+}
