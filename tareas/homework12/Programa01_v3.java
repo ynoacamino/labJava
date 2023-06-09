11c4d8fff233727b4b94e654677c7e1a7321f639
Diegoo11 <ynoacamino@gmail.com>
2023-06-08 21:03:36 -0500

    homework12

---
+++ tareas/homework12/Programa01_v3.java
@@ -0,0 +1,13 @@
+package tareas.homework12;
+import java.util.*;
+
+public class Programa01_v3 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int num = sc.nextInt();
+
+    for(int i = 1; i <= num; i += 1) {
+      System.out.println(i);
+    }
+  }
+}
