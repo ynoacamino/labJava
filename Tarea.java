bb433f19692ccbdfb9cde9374be13d39e70485bf
Diegoo11 <ynoacamino@gmail.com>
2023-06-12 18:50:00 -0500

    tarea de FP

--- Tarea.java
+++ Tarea.java
@@ -1,5 +1,5 @@
 // programa que hace operaciones matematicas en el metodo de salida
-class Tarea {
+public class Tarea {
   public static void main(String[] args) {
     System.out.println("El producto de 3 por 8 es " + 3*8);
     System.out.println("El profucto de 3 por 8 mas 7 es " + (3*8+7));
