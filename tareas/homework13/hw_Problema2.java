f5615a024095f8e3f312e8d5458a5805685777da
Diegoo11 <ynoacamino@gmail.com>
2023-06-13 21:00:04 -0500

    homework for nicol

--- tareas/homework13/hw_Problema2.java
+++ tareas/homework13/hw_Problema2.java
@@ -6,19 +6,26 @@ public class hw_Problema2 {
     Scanner sc = new Scanner(System.in);
     int num1 = sc.nextInt();
     int num2 = sc.nextInt();
-    int max = Math.max(num1, num2);
-    int min = Math.min(num1, num2);
+    int a;
+    int b;
 
-    int resto;
-
-    while(true) {
-      resto = max % min;
-      if(resto == 0) break;
-      max = min;
-      min = resto;
+    if(num1> num2 ) {
+      a = num1;
+      b = num2;
+    } else {
+      b = num1;
+      a = num2;
     }
-    System.out.println("El MDC es: " + min);
-
+    int x;
+    while(true) {
+      x = a % b;
+      if(x == 0) {
+        break;
+      }
+      a = b;
+      b = x;
+    }
+    System.out.println(b);
   }
 }
 
