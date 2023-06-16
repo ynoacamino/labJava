d1ab7335d30021d14630ca8aaf8c3a10cf435bf4
Diegoo11 <ynoacamino@gmail.com>
2023-06-15 21:09:48 -0500

    homework

---
+++ tareas/homework14/Hw_Problema2.java
@@ -0,0 +1,22 @@
+package tareas.homework14;
+import java.util.*;
+
+public class Hw_Problema2 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int positivos = 0;
+    int negativos = 0;
+
+
+    while(true) {
+      int num = sc.nextInt();
+      if( num == 0 ) break;
+      if( num > 0) positivos += 1;
+      else negativos += 1;
+    }
+
+    System.out.println("Positivos: " + positivos);
+    System.out.println("Negativos : " + negativos);
+
+  }
+}
