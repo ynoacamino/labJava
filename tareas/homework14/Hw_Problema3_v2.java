15815b7c715d8091a74533418a08d2665e2b2888
Diegoo11 <ynoacamino@gmail.com>
2023-06-15 21:09:51 -0500

    homework

---
+++ tareas/homework14/Hw_Problema3_v2.java
@@ -0,0 +1,26 @@
+package tareas.homework14;
+import java.util.*;
+
+public class Hw_Problema3_v2 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int min, max, intentos = 0;
+    boolean cicle = true;
+
+    do {
+      System.out.println("Ingresa el menor numero");
+      min = sc.nextInt();
+      System.out.println("Ingresa el mayor numero");
+      max = sc.nextInt();
+    } while(min > max);
+
+    System.out.println("Ingresa un numero dentro del rango indicado: ");
+    while(cicle) {
+      intentos += 1;
+      int num = sc.nextInt();
+      if(num <= max && num >= min) cicle = false;
+    }
+
+    System.out.println("Intentos: " + intentos);
+  }
+}
