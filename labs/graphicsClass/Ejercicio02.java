a9821c9de18d7f90abedb2c7a83688ce593bb9fc
Diegoo11 <ynoacamino@gmail.com>
2023-07-05 22:01:56 -0500

    homework

---
+++ labs/graphicsClass/Ejercicio02.java
@@ -0,0 +1,17 @@
+package graphicsClass;
+import graphics.*;
+
+public class Ejercicio02 {
+    public static void main(String[] args) {
+        Picture cellWhite = Picture.casilleroBlanco();
+        Picture cellBlack = Picture.casilleroBlanco().invertir();
+        Picture rowTable = cellWhite.alLado(cellBlack)
+            .alLado(cellWhite).alLado(cellBlack)
+            .alLado(cellWhite).alLado(cellBlack)
+            .alLado(cellWhite).alLado(cellBlack)
+            .invertir(); // Linea agregada para inveritir los colores y no tener que crear desde cero
+
+        Graphics display = new Graphics(rowTable);
+        display.print();
+    }
+}
