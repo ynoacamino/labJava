cd28dc3c05a40d10320e0d083e009f76b9f8845f
Diegoo11 <ynoacamino@gmail.com>
2023-07-05 20:01:00 -0500

    poo

---
+++ poo/Ave.java
@@ -0,0 +1,25 @@
+package poo;
+
+public class Ave {
+  // PROPIEDADES
+  String sound;
+  boolean fly;
+
+  //    CONSTRUCTOR
+  public Ave (String sonido, boolean volar) {
+    this.sound = sonido;
+    this.fly = volar;
+  }
+
+  // METODOS
+  public static void Picar() {
+    System.out.println("Picó");
+  }
+
+  public boolean getFly() {
+    return fly;
+  }
+  public String getSound() {
+    return sound;
+  }
+}
