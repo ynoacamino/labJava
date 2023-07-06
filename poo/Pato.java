96055c718088fb850f4ecaf4d10e22d7a3b72dce
Diegoo11 <ynoacamino@gmail.com>
2023-07-05 20:01:03 -0500

    poo

---
+++ poo/Pato.java
@@ -0,0 +1,29 @@
+package poo;
+import poo.Ave;
+
+public class Pato extends Ave {
+  private String color;
+  private int age;
+  private double size;
+
+  public Pato (String sonido, boolean volar, String color, int edad, double tamano) {
+    super(sonido, volar);
+    this.color = color;
+    this.age = edad;
+    this.size = tamano;
+  }
+
+  public String getColor() {
+    return color;
+  }
+  public int getAge() {
+    return age;
+  }
+  public double getSize() {
+    return size;
+  }
+  public void setColor(String color){
+    this.color = color;
+  }
+
+}
