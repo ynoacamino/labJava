d500072767474da55ae21c000cfdcb8c5e31db57
Diegoo11 <ynoacamino@gmail.com>
2023-07-07 18:19:11 -0500

    homework

--- labs/graphicsClass/tareaAjedresJava/Main.java
+++
@@ -1,8 +0,0 @@
-import graphics.*;
-class Main{
-  public static void main(String[] args){
-    Picture casilleroNegro = Picture.casilleroBlanco().invertir();
-    Graphics g = new Graphics(casilleroNegro);
-    g.print();
-  }
-}
